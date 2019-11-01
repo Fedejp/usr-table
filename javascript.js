8c425ce511d4e80a23bbfd9b7a172aac53a5ef64
Fedejp <fedejp_98@hotmail.com.ar>
2019-11-01 09:42:09 -0300

    Agregado guardar en LocalStorage

--- javascript.js
+++ javascript.js
@@ -1,5 +1,30 @@
 
+
 $(document).ready(function(){
+/*         //Load cookies
+        var cookies = document.cookie.split(";");
+        console.log(cookies);
+        for(let i = 0; i<cookies.length; i++){
+            //let rowCookie = JSON.parse(cookies[i]);
+            let rowTable = "<tr><td>"+rowCookie[0]+"</td><td>"+rowCookie[1]+"</td><td>"+rowCookie[2]+" años</td><td>"+rowCookie[3]+"</td><td>"+rowCookie[4]+"</td><td><button type=edit class=\"btn btn-link editar\" data-toggle=modal data-target=#usrInput><span class=\"glyphicon glyphicon-pencil\"></span></button><button type=delete class=\"btn btn-danger borrar\"><span class=\"glyphicon glyphicon-remove-circle editar\"</span></button></td></tr>"; 
+            let _htmlrow = document.createElement("TR");
+            _htmlrow.innerHTML = rowTable;
+            _htmlrow.setAttribute("id",""+rowCookie[0]);
+        }
+         */
+    //Load from LocalStorage
+    for(let i = 1; i<=localStorage.length; i++){
+        console.log(localStorage.getItem("usr"+i))
+        let rowStoraged = localStorage.getItem("usr"+i).split(",");
+        console.log(rowStoraged);
+        let _age = getAge(rowStoraged[2])
+        let rowTable = "<tr><td>"+rowStoraged[0]+"</td><td>"+rowStoraged[1]+"</td><td>"+_age+" años</td><td>"+rowStoraged[3]+"</td><td>"+rowStoraged[4]+"</td><td><button type=edit class=\"btn btn-link editar\" data-toggle=modal data-target=#usrInput><span class=\"glyphicon glyphicon-pencil\"></span></button><button type=delete class=\"btn btn-danger borrar\"><span class=\"glyphicon glyphicon-remove-circle editar\"</span></button></td></tr>"; 
+        let _htmlrow = document.createElement("TR");
+        _htmlrow.innerHTML = rowTable;
+        _htmlrow.setAttribute("id",""+rowStoraged[0]);
+        document.getElementById("tableBody").appendChild(_htmlrow);
+    }
+    
     //table pager
     $('#data').after('<div id="nav" class="col-md-12 text-center"></div>');
     var rowsShown = 5;
@@ -43,50 +68,43 @@ function save(){
             var _sexchosen = _sexradio[i].value;
     }
     let _mail= document.getElementById("inputEmail").value;
-    let _age = getAge();
+    let _age = getAge(_dob);
 
     if(document.getElementById("defrow")!=null){
         var row = document.getElementById("defrow");
         row.parentNode.removeChild(row);
     }
-        
-    
-
-
-  
-    
-
     let _id=  $('#data tbody tr').length+1;
-    
     let _row = "<tr><td>"+_id+"</td><td>"+_name+"</td><td>"+_age+" años</td><td>"+_sexchosen+"</td><td>"+_mail+"</td><td><button type=edit class=\"btn btn-link editar\" data-toggle=modal data-target=#usrInput><span class=\"glyphicon glyphicon-pencil\"></span></button><button type=delete class=\"btn btn-danger borrar\"><span class=\"glyphicon glyphicon-remove-circle editar\"</span></button></td></tr>";
     let _htmlrow = document.createElement("TR");
     _htmlrow.innerHTML = _row;
     _htmlrow.setAttribute("id",""+_id);
     document.getElementById("tableBody").appendChild(_htmlrow);
-
-    function getAge() {
-         //getting the values for the Date of birth
-        let _dobarr = _dob.split("-");
-        let _doby = _dobarr[0];
-        let _dobm = _dobarr[1];
-        let _dobd = _dobarr[2];
-        //getting the values for today
-        let _today = new Date();
-        let _todayy = _today.getFullYear();
-        let _todaym = _today.getMonth() + 1;
-        let _todayd = _today.getDate();
-        //calculates age in years
-        let _age = _todayy - _doby;
-        if (_todaym < _dobm) {
-            _age--;
-        }
-        if ((_dobm == _todaym) && (_todayd < _dobd)) {
-            _age--;
-        }
-        return _age;
-    }
+    let _arraydatos = [_id,_name, _dob, _sexchosen, _mail];
+    //$.cookie= ("usr"+_id, JSON.stringify(_arraydatos)); 
+    localStorage.setItem("usr"+_id,_arraydatos);
 }
-
+function getAge(_dob) {
+    //getting the values for the Date of birth
+   let _dobarr = _dob.split("-");
+   let _doby = _dobarr[0];
+   let _dobm = _dobarr[1];
+   let _dobd = _dobarr[2];
+   //getting the values for today
+   let _today = new Date();
+   let _todayy = _today.getFullYear();
+   let _todaym = _today.getMonth() + 1;
+   let _todayd = _today.getDate();
+   //calculates age in years
+   let _age = _todayy - _doby;
+   if (_todaym < _dobm) {
+       _age--;
+   }
+   if ((_dobm == _todaym) && (_todayd < _dobd)) {
+       _age--;
+   }
+   return _age;
+}
 
 $(document).on('click', '.borrar', function (event) { //función para que una fila se borre a sí misma
     event.preventDefault();
@@ -109,4 +127,3 @@ $(document).on('click', '.editar', function (event) { //función para que una fi
     document.getElementById
 
 });
-
